74fec1d3b04982983642af2d64dc39825ade190d
koneu <koneu93@googlemail.com>
2013-05-29 12:15:52 +0200

    added ability to run shell in bg, added comments, fixed gitignore so it does not ignore itself

--- jni/sudo.c
+++ jni/sudo.c
@@ -2,15 +2,32 @@
 #include <stdlib.h>
 #include <jni.h>
 
+static FILE *shell;
+
 jboolean
 Java_koneu_rootapp_MainActivity_sudo(JNIEnv *env, jobject obj, jstring xmd) {
 	char *cmd;
-	jboolean ret;
-	asprintf(&cmd, "su -c \"%s\"", ((*env)->GetStringUTFChars(env, xmd, 0)));
-	if(system(cmd) == EXIT_SUCCESS) {
-		ret = JNI_TRUE;
+	jboolean ret = JNI_TRUE;
+	if(shell) {
+		fprintf(shell, "%s\n", ((*env)->GetStringUTFChars(env, xmd, 0)));
 	} else {
-		ret = JNI_FALSE;
+		asprintf(&cmd, "su -c \"%s\"", ((*env)->GetStringUTFChars(env, xmd, 0)));
+		if(system(cmd) != EXIT_SUCCESS) {
+			ret = JNI_FALSE;
+		}
 	}
 	return ret;
+}
+
+void
+Java_koneu_rootapp_MainActivity_startshell(JNIEnv *env, jobject obj) {
+	if(!shell) {
+		shell = popen("su", "w");
+	}
+}
+
+void
+Java_koneu_rootapp_MainActivity_closeshell(JNIEnv *env, jobject obj) {
+	pclose(shell);
+	shell = NULL;
 }
--- src/koneu/rootapp/MainActivity.java
+++ src/koneu/rootapp/MainActivity.java
@@ -5,21 +5,38 @@ import android.os.Bundle;
 import koneu.rootapp.R;
 
 public class MainActivity extends Activity {
-
+	
 	static {
 		System.loadLibrary("sudo");
 	}
-
-	public native boolean sudo(String cmd);
-
+	
+	final public native boolean sudo(String cmd); /* This will execute "cmd" in the shell started with startshell() or via "su -c "cmd"". */
+	final public native void startshell(); /* This will start a new shell in the background, it WILL NOT DETECT WHETHER PRIVILEGES HAVE BEEN GRANTED. If a shell is running this is a no-op. */
+	final public native void closeshell(); /* This will close the shell started with startshell(). */
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	final protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (sudo("true")) {
+		
+		startshell(); /* THIS IS WRONG. DO NOT DO IT LIKE THIS, do it like shown below. */
+		
+		if (sudo("true")) { /* Check if we have SU privileges. */
+			startshell(); /* If permission has been granted, start a shell in the background. */
+			/* From now on, all calls to sudo() will be passed to the shell instead of run individually. */
+			
 			setContentView(R.layout.activity_main);
+			
+			startshell(); /* Does nothing, because we already have a shell running. */
+			
 		} else {
 			setContentView(R.layout.no_root);
 		}
 	}
-
+	
+	@Override
+	final protected void onDestroy() {
+		super.onDestroy();
+		closeshell(); /* Close the shell when closing the app. */
+	}
+	
 }
