3184186cc469bad95fab6d48acf447f07a4f9a5c
koneu <koneu93@googlemail.com>
2013-06-05 09:37:05 +0200

    I was really really stupid.

--- jni/sudo.c
+++ jni/sudo.c
@@ -9,22 +9,22 @@ static processimage *process;
 
 jboolean
 Java_koneu_rootapp_MainActivity_sudo(JNIEnv *env, jobject obj, jstring xmd) {
-	const char *cmd = ((*env)->GetStringUTFChars(env, xmd, 0));
+	char const * const cmd = (*env)->GetStringUTFChars(env, xmd, 0), *rmd = cmd;
 	jboolean ret = JNI_TRUE;
 	if(process) {
-		while(cmd[0]  != 0) {
-			if(write(process->infd, cmd, 1) != 1) {
+		while(rmd[0]  != 0) {
+			if(write(process->infd, rmd, 1) != 1) {
 				ret = JNI_FALSE;
 				goto END;
 			}
-			cmd++;
+			rmd++;
 		}
 		if(write(process->infd, "\n", 1) != 1)
 			ret = JNI_FALSE;
 	} else {
 		char *buf;
 		asprintf(&buf, "su -c \"%s\"", cmd);
-		if(system(cmd) != EXIT_SUCCESS)
+		if(system(buf) != EXIT_SUCCESS)
 			ret = JNI_FALSE;
 	}
 	END:
